f53e25f52dfaf10c7f1dd48f2ec236d1fecca2a8
Lucas Nabesima <lucas.nabesima@gmail.com>
2022-10-21 07:56:00 -0300

    feat: add NavbarData context

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,17 @@
 import '../styles/globals.css';
 import { Layout } from '../components/Layout/Layout';
 import { PostsProvider } from '../context/PostsData';
+import { NavbarProvider } from '../context/NavbarData';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <PostsProvider>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </PostsProvider>
+    <NavbarProvider>
+      <PostsProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </PostsProvider>
+    </NavbarProvider>
   );
 }
 
